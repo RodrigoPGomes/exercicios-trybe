d81c434834bf536e593dc471df03f3ea6c3afb43
Rodrigo Gomes <digogomes@gmail.com>
2022-05-17 23:15:00 -0300

    exercicio 01 bloco 4 dia 01

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/index.js
@@ -0,0 +1,27 @@
+// exercicio 01
+
+const numeroA = 10;
+const numeroB = 2;
+let resultado = null;
+
+switch (resultado)
+{
+    
+    case resultado: numeroA + numeroB; 
+    console.log('Resultado da soma = ', numeroA + numeroB);
+
+    case resultado: numeroA - numeroB;
+    console.log('Resultado da subtracao = ', numeroA - numeroB);
+
+    case resultado: numeroA * numeroB;
+    console.log('Resultado da multiplicacao = ', numeroA * numeroB);
+
+    case resultado: numeroA / numeroB;
+    console.log('Resultado da divisao = ', numeroA / numeroB);
+
+    case resultado: numeroA % numeroB;
+    console.log('Resultado do modulo = ', numeroA % numeroB);
+
+}
+
+

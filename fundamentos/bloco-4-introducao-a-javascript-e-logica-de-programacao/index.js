f060bbae414771cc81545261bd3bc13ec61b80db
Rodrigo Gomes <digogomes@gmail.com>
2022-05-17 23:26:50 -0300

    feito exercico 02 do bloco 4 dia 1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/index.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/index.js
@@ -1,6 +1,6 @@
-// exercicio 01
+// exercicio 01 - 
 
-const numeroA = 10;
+/* const numeroA = 10;
 const numeroB = 2;
 let resultado = null;
 
@@ -22,6 +22,17 @@ switch (resultado)
     case resultado: numeroA % numeroB;
     console.log('Resultado do modulo = ', numeroA % numeroB);
 
-}
+}*/
 
+
+// exercicio 02 - Imprimir o maior de dois numeros
+
+const primeiroNumero = 35;
+const segundoNumero = 55;
+
+if (primeiroNumero > segundoNumero){
+    console.log('Primeiro número é maior que o segundo!');
+}else{
+    console.log('O segundo número é maior que o primeiro!');
+}
 
